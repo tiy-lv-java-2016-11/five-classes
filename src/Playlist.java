2ea2a9fb4770c43fda6d7be2beafce78390c14bd
GreedOrDie <greedordie@gmail.com>
2016-12-01 15:26:13 -0800

    This is my resubmitted version of five-classes

--- src/Playlist.java
+++ src/Playlist.java
@@ -2,7 +2,7 @@
  * //This is a list of songs, that has 34 genres.
  */
 public class Playlist {
-    int [] songGenre = new int [34];
+    int [] songGenres = new int [34];
     int songCount;
     String songName = "";
     int songLength;
